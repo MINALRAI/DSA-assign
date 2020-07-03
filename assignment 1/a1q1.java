42dcae50a1c5093903d617ec33c35dd6a26d8ad4
Ankit Raj Biswal <ankitrudra2001@gmail.com>
2020-07-03 07:50:51 +0530

    Update a1q1.java

--- assignment 1/a1q1.java
+++ assignment 1/a1q1.java
@@ -4,13 +4,13 @@ class a1q1
 {
     public static void main(String args[])
     {
-        Scanner in = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Value of a: ");
-        int a=in.nextInt();
+        int a=sc.nextInt();
         System.out.print("Enter the value of b: ");
-        int b=in.nextInt();
+        int b=sc.nextInt();
         System.out.print("Enter the value of c: ");
-        int c=in.nextInt();
+        int c=sc.nextInt();
         boolean k=false;
         if(a+b==c || a==b-c || a*b==c)
             k=true;
